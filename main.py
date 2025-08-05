7af3e3bf1fae578d5a12cd3bab9250a71f893552
Valeriy Nasyrov <valerius.nasyrov@yandex.ru>
2025-08-05 20:52:11 +0300

    'п -> П' change

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from selene import browser, be, have
 
 def test_input_value():
     browser.open('https://ya.ru')
-    browser.element('[id="text"]').type('погода Санкт-Петербург').press_enter()
+    browser.element('[id="text"]').type('Погода Санкт-Петербург').press_enter()
     browser.element('input[name="text"]').should(have.value('погода Санкт-Петербург'))
